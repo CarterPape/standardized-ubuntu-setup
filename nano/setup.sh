df4e91e38aa3585e0ba90101b96f20035417ba86
Carter Pape <carterpape@gmail.com>
2020-01-16 16:51:26 -0700

    better naming

--- nano/setup.sh
+++ nano/setup.sh
@@ -2,8 +2,8 @@ sudo apt install nano
 
 sudo mv /etc/nanorc /etc/nanorc.old
 
-cp "./for ~:/*" ~/
-sudo cp "./for :usr:local:etc:/*" /usr/local/etc/
+cp ./for-home./* ~/
+sudo cp ./for-.usr.local.etc./* /usr/local/etc/
 
 sudo mv /bin/nano /bin/nano.old
 sudo ln -s /usr/local/bin/nano /bin/nano
