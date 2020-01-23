2763d6d15025ea2ee3b26f02114b6048eb95fa62
Carter Pape <carterpape@gmail.com>
2020-01-23 16:13:24 -0700

    re-ups the nano install

--- nano/setup.sh
+++ nano/setup.sh
@@ -1,4 +1,23 @@
-sudo apt install nano
+# prerequisites
+
+sudo apt install make
+sudo apt install clang
+sudo apt install gcc
+sudo apt install libncurses5-dev libncursesw5-dev
+sudo apt install groff
+sudo apt install pkg-config
+
+# nano
+
+curl -O https://www.nano-editor.org/dist/v4/nano-4.7.tar.gz
+tar -xvf nano-4.7.tar.gz
+
+cd nano-4.7
+./configure
+make
+sudo make install
+
+# shell game
 
 sudo mv /etc/nanorc /etc/nanorc.old
 
