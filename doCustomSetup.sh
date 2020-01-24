d403177921714702e0527e18c021c869f273f7c9
Carter Pape <carterpape@gmail.com>
2020-01-23 17:56:36 -0700

    tries to avoid ssh cert problem

--- doCustomSetup.sh
+++ doCustomSetup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 read -p "What would you like to be the hostname of this machine? " newhostname
-sudo hostnamectl set-hostname $newhostname
 
 echo "Setting timezone do America/Denver"
 sudo timedatectl set-timezone America/Denver
 
 sudo apt update -yq
+sudo apt -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install openssh_server
 sudo apt full-upgrade -yq
 sudo apt autoremove -yq
 
@@ -15,6 +15,9 @@ cd zsh
 
 cd ../nano
 . ./setup.sh
+
+echo "Setting hostname to $newhostname"
+sudo hostnamectl set-hostname $newhostname
 
 read -p "All done. Press [Enter] to reboot."
 sudo reboot
