32c19b056e6f3d012e39718904ad6f6b308529d6
Carter Pape <carterpape@gmail.com>
2020-01-23 21:46:11 -0700

    attempts to fix input prompts

--- doCustomSetup.sh
+++ doCustomSetup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+while read -r -t 0; do read -r; done
 read -p "What would you like to be the hostname of this machine? " newhostname
 
 echo "Setting timezone do America/Denver"
 sudo timedatectl set-timezone America/Denver
 
+export DEBIAN_FRONTEND=noninteractive
 sudo apt update -yq
-sudo apt -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install openssh_server -yq
+sudo apt -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install openssh-server -yq
 sudo apt full-upgrade -yq
 sudo apt autoremove -yq
 
