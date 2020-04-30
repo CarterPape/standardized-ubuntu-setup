cdd46d10782cfde4a65242992746fd18a2548e04
Carter Pape <public@carterpape.com>
2020-04-29 23:42:39 -0600

    tries to update

--- pip/setup.sh
+++ pip/setup.sh
@@ -5,10 +5,10 @@ sudo apt-get install python3-distutils -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 
 sudo usermod -a -G staff $USER
-newgrp staff
-newgrp $USER
 sudo chown root:staff /usr/local/bin
 sudo chmod g+w /usr/local/bin
 sudo chown $USER -R /usr/local/lib/python3*/dist-packages
 
 python3 get-pip.py
+
+pip completion --zsh >> ~/.zprofile
